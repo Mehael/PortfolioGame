0b2771360395f04e2b58f7c3d9b946db4d33b20d
Mikhail Petrov <mehael@mail.ru>
2022-07-27 18:49:44 +1000

    Add color randomizer

---
+++ Assets/1.MemoryPuzzle/ColorChanger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using Random = UnityEngine.Random;
+
+public class ColorChanger : MonoBehaviour
+{
+    public VolumeProfile PostProcessing;
+
+    private IEnumerator Start()
+    {
+        var check = PostProcessing.TryGet<Bloom>(out var bloom);
+        if (!check)
+            yield break;
+
+        while (true) {
+            var timer = Random.Range(.5f, 5f);
+            var t = 0f;
+            var nextColor = Random.ColorHSV(0,1,.5f,.8f);
+            var currentColor = bloom.tint.value;
+            while (t < timer)
+            {
+                bloom.tint.value = Color.Lerp(currentColor, nextColor, t / timer);
+                yield return new WaitForEndOfFrame();
+                t += Time.deltaTime;
+            }  
+        }
+
+        
+    }
+}
